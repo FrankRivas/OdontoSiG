a64445dfd475b50c7dd654be8fa0a71f8c57000a
Andrés Orellana <andres.aeom@gmail.com>
2018-06-13 00:22:33 -0600

    Previo a merge de primer reporte estrategico

---
+++ gerencial/static/gerencial/js/reporte_pac_atendidos.js
@@ -0,0 +1,74 @@
+var datos = [];
+
+for(var i = 0; i < filas.length; i++) {
+    datos.push({
+        name: filas[i][0],
+        y: filas[i][1]
+    });
+}
+
+Highcharts.setOptions({
+    lang: {
+        downloadPNG: "Descargar en formato PNG",
+        downloadPDF: "Descargar en formato PDF",
+        downloadJPEG: "Descargar en formato JPEG"
+    }
+});
+Highcharts.chart("container",{
+        chart: {
+            //Tipo de gráfico y espacio de header
+            type:"pie",
+            spacingTop: 200
+        },
+        //Mantiene el formato de html en la exportación
+        exporting:{
+            enabled:"true",
+            allowHTML:"true",
+            buttons: {
+                contextButton: {
+                    menuItems: [ "downloadPNG", "downloadPDF", "downloadJPEG"]
+                }
+            }
+        },
+        plotOptions:{
+            //Tamaño del diámetro del gráfico
+            pie: {
+                size:250
+            },
+            //Opciones de labels en el gráfico
+            series: {
+                dataLabels:{
+                    align:"left",
+                    enabled:true,
+                    formatter: function() {
+                        return this.point.name+':'+this.percentage.toFixed(1)+'%';
+                    }
+                }
+            }
+        },
+        legend:{
+            enabled:true,
+            align:"center"
+        },
+        //Información en base a la que se construye el gráfico
+        series: [{
+            showInLegend:true,
+            name: "Parámetro de dato",
+            data:datos
+        }]
+},function (chart) {
+    chart.renderer.image(logo, 30, 25, 130, 130).add();
+    chart.renderer.text("UNIVERSIDAD DE EL SALVADOR", 200, 60).css({
+        fontSize: "18px"
+    }).add();
+    chart.renderer.text('FACULTAD DE ODONTOLOGÍA', 200, 90).css({
+        fontSize: "18px"
+    }).add();
+    chart.renderer.text('SCSAB-FOUES', 200, 120).css({
+        fontSize: "18px"
+    }).add();
+    chart.setTitle({text:"<div class='col s12'>" +
+        "<div class='offset-s4'><p class='center-align'>Informe de pacientes atendidos</p></div></div>"});
+    chart.setSubtitle({text:"<div class='col s12'>" +
+        "<div class='offset-s3'><p class='center-align'>Desde: " + " hasta: </p></div></div>"});
+});
