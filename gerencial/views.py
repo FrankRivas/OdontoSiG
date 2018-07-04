b440d73b090a6be61f9a8a8d73d7707afe8163e8
Frank Rivas <cfrivas2010@hotmail.com>
2018-07-04 15:48:06 -0600

    Img64 with js

--- gerencial/views.py
+++ gerencial/views.py
@@ -63,29 +63,19 @@ def reporte_bitacora(request):
         acciones = acciones.filter(FechaAccion__lte=b[6:] + "-" + b[3:5] + "-" + b[:2])
 
     #acciones = Bitacora.objects.all()
-    img = "static/gerencial/img/encabezado.png"
-    with open(img, 'rb') as f:
-        contents = f.read()
-        base = base64.b64encode(contents)
-        image = "data:image/png;base64," + base.decode('utf-8')
 
     context = {
         'acciones': acciones,
-        'image':image,
+
     }
 
     return render(request, 'reporte_bitacora.html', context)
 
 def reporte_conexion(request):
     conexiones = User.objects.all()
-    img = "static/gerencial/img/encabezado.png"
-    with open(img, 'rb') as f:
-        contents = f.read()
-        base = base64.b64encode(contents)
-        image = "data:image/png;base64," + base.decode('utf-8')
     context = {
         'conexiones': conexiones,
-        'image' : image,
+
     }
 
     return render(request, 'reporte_conexiones.html', context)
